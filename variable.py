44c1b0721d6d9164c007c8bd33ef7d0baf1d5a86
MD. Rakibul Islam <rakibulislam.cse.41230301525@gmail.com>
2025-10-21 22:02:13 +0600

    n

---
+++ variable.py
@@ -0,0 +1,16 @@
+a=1
+b='Rakib'
+c=True
+d=None
+print(type(a))
+print(type(b))  
+print(type(c))
+print(type(d))
+
+
+list1=[1,2,3,4,5,'banana','apple','orange']
+print(type(list1))
+tuple1=(1,2,3,4,5,'banana','apple','orange')
+print(tuple1)
+dict1={'name':'Rakib','age':23,'city':'Dhaka'}
+print(dict1)
