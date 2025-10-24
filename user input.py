e6d1daa575d1e151f9c9aa195f839d1bbacc567d
MD. Rakibul Islam <rakibulislam.cse.41230301525@gmail.com>
2025-10-24 22:03:49 +0600

    new

---
+++ user input.py
@@ -0,0 +1,9 @@
+a = input('Enter your input: ') 
+print('You entered:', a)
+
+x = input('Enter first input: ')
+y = input('Enter second input: ')
+
+
+print('Concatenated inputs:', x + y)
+print('Sum of inputs as integers:', int(x) + int(y))
